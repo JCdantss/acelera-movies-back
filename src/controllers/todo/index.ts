7f3befd1a841a200094229bdd9cb60ce5b613c84
Julio Cesar <juliocesardantss@gmail.com>
2022-08-02 14:25:34 -0300

    criando endpoint

--- src/controllers/todo/index.ts
+++ src/controllers/todo/index.ts
@@ -1,3 +1,6 @@
+import { getRepository } from "typeorm"
+import { Movies } from "@models/entity/Movies"
+import { User } from "@models/entity/User"
 
 export const itsWorks = (request, response) => {
   return response.json({ message: "It's Works!!" })
@@ -5,4 +8,64 @@ export const itsWorks = (request, response) => {
 
 export const getTODO = (request, response) => {
   return response.json({ todos: [] })
+}
+export const getMovies = async (request, response) => {
+  try {
+    const movieRepository = getRepository(Movies)
+    const findMovie = await movieRepository.find()
+    return response.status(200).json(findMovie)
+  } catch (error) {
+    return response.status(500).json(error)
+  }
+}
+export const getLogin = async (request, response) => {
+  const userRepository = getRepository(User)
+  const findUser = await userRepository.find()
+  return response.json(findUser)
+}
+
+export const getMoviesId = async (request, response) => {
+  try {
+    const { id } = request.params
+    const movieRepository = getRepository(Movies)
+    const findMovie = await movieRepository.findOne(id)
+
+    return response.status(200).json(findMovie)
+  } catch (error) {
+    return response.status(500).json(error)
+  }
+}
+
+export const postMoviesId = async (request, response) => {
+  try {
+    const {
+      title,
+      subtitle,
+      resume,
+      releaseDate,
+      image,
+      director,
+      writer,
+      classification,
+      studio,
+      note,
+    } = request.body
+    const movieRepository = getRepository(Movies)
+    const findMovie = movieRepository.create({
+      title,
+      subtitle,
+      resume,
+      releaseDate,
+      image,
+      director,
+      writer,
+      classification,
+      studio,
+      note,
+    })
+    const saveMovies = await movieRepository.save(findMovie)
+    return response.status(200).json(saveMovies)
+  } catch (error) {
+    return response.status(500).json(error)
+  }
 }
