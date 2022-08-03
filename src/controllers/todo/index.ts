f3096b2e34c4154cd7066daa876bbd9d511f1784
Julio Cesar <juliocesardantss@gmail.com>
2022-08-03 11:35:33 -0300

    criando endpoint put

--- src/controllers/todo/index.ts
+++ src/controllers/todo/index.ts
@@ -2,13 +2,6 @@ import { getRepository } from "typeorm"
 import { Movies } from "@models/entity/Movies"
 import { User } from "@models/entity/User"
 
-export const itsWorks = (request, response) => {
-  return response.json({ message: "It's Works!!" })
-}
-
-export const getTODO = (request, response) => {
-  return response.json({ todos: [] })
-}
 export const getMovies = async (request, response) => {
   try {
     const movieRepository = getRepository(Movies)
@@ -51,7 +44,7 @@ export const postMoviesId = async (request, response) => {
       note,
     } = request.body
     const movieRepository = getRepository(Movies)
-    const findMovie = movieRepository.create({
+    const createMovie = movieRepository.create({
       title,
       subtitle,
       resume,
@@ -63,8 +56,71 @@ export const postMoviesId = async (request, response) => {
       studio,
       note,
     })
-    const saveMovies = await movieRepository.save(findMovie)
+    const saveMovies = await movieRepository.save(createMovie)
     return response.status(200).json(saveMovies)
+  } catch (error) {
+    return response.status(500).json(error)
+  }
+}
+export const deleteMoviesId = async (request, response) => {
+  try {
+    const { id } = request.params
+    const getMovies = getRepository(Movies)
+    const deleteMoviesId = await getMovies.delete(id)
+    return response.status(200).json(deleteMoviesId)
+  } catch (error) {
+    return response.status(500).json(error)
+  }
+}
+export const moviesPut = async (request, response) => {
+  try {
+    const {
+      title,
+      subtitle,
+      resume,
+      releaseDate,
+      image,
+      director,
+      writer,
+      classification,
+      studio,
+      note,
+    } = request.body
+    const { id } = request.params
+    const updateMoviesId = getRepository(Movies)
+    const movie = await updateMoviesId.findOne(id)
+    if (title) {
+      movie.title = title
+    }
+    if (subtitle) {
+      movie.subtitle = subtitle
+    }
+    if (resume) {
+      movie.resume = resume
+    }
+    if (releaseDate) {
+      movie.releaseDate = releaseDate
+    }
+    if (image) {
+      movie.image = image
+    }
+    if (director) {
+      movie.director = director
+    }
+    if (writer) {
+      movie.writer = writer
+    }
+    if (classification) {
+      movie.classification = classification
+    }
+    if (studio) {
+      movie.studio = studio
+    }
+    if (note) {
+      movie.note = note
+    }
+    const result = await updateMoviesId.save(movie)
+    return response.status(200).json(result)
   } catch (error) {
     return response.status(500).json(error)
   }
--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,17 @@
 import {
-  getTODO,
-  itsWorks,
+  deleteMoviesId,
   getMovies,
   getMoviesId,
+  moviesPut,
   postMoviesId,
 } from "@controllers/todo"
 import { getLogin } from "@controllers/todo"
 
 export const defineRoutes = (app) => {
-  app.get("/", itsWorks)
-  app.get("/todo", getTODO)
+  app.get("/login", getLogin)
   app.get("/movies", getMovies)
   app.get("/movies/:id", getMoviesId)
-  app.get("/login", getLogin)
+  app.delete("/movies/:id", deleteMoviesId)
   app.post("/movies", postMoviesId)
+  app.put("/movies/:id", moviesPut)
 }
